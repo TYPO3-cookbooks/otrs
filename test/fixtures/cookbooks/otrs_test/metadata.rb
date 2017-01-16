2068e729463f4385a83c6bcd7e79d2e6bac4f9d5
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-16 17:04:50 +0100

    [TESTS] Add inspec tests for HTTP

---
+++ test/fixtures/cookbooks/otrs_test/metadata.rb
@@ -0,0 +1,8 @@
+name             "otrs_test"
+maintainer       "TYPO3 Association"
+maintainer_email "steffen.gebert@typo3.org"
+license          "Apache 2.0"
+description      "Tests for otrs"
+version          "0.0.0"
+
+depends "hostsfile", "= 2.4.5"
---
+++ test/fixtures/cookbooks/otrs_test/recipes/hostname.rb
@@ -0,0 +1,18 @@
+# does not work inside docker, until this is released:
+# https://github.com/customink-webops/hostsfile/pull/78
+# Current version 2.4.5 doesn't contain this.
+
+#hostsfile_entry node['ipaddress'] do
+#  hostname 'review.vagrant'
+#  aliases ['git.vagrant']
+#end
+
+ruby_block "update /etc/hosts" do
+  block do
+    fe = Chef::Util::FileEdit.new("/etc/hosts")
+    fe.insert_line_if_no_match(/otrs\.vagrant/,
+                               "#{node['ipaddress']} #{node['hostname']} otrs.vagrant")
+    fe.write_file
+  end
+  not_if { Resolv.getaddress('otrs.vagrant') rescue false }
+end
