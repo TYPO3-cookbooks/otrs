cafa24495077c1ad3a6e0269ca9cb095a486cb32
Steffen Gebert <steffen.gebert@typo3.org>
2017-02-10 13:31:09 +0100

    [TASK] Switch to v2 pipeline

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,8 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Deploy and configure OTRS (Open Ticket Request System)."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 source_url       "https://github.com/TYPO3-cookbooks/otrs" if respond_to?(:source_url)
-
-version          "2.1.0"
 
 supports         "debian"
 
